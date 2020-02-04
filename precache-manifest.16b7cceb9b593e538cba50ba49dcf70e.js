0bd8c9886570b0e0d4c92ecce310a4d85713d076
shreyas <>
2020-02-04 18:53:18 +0530

    Updates

---
+++ precache-manifest.16b7cceb9b593e538cba50ba49dcf70e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9ce9656116fe7cee39c49ff36b0d91",
+    "url": "/my_site/index.html"
+  },
+  {
+    "revision": "f614615c1bc81af62f9a",
+    "url": "/my_site/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e1bb425fa67a7ee1abad",
+    "url": "/my_site/static/js/2.6d5513b7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my_site/static/js/2.6d5513b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f614615c1bc81af62f9a",
+    "url": "/my_site/static/js/main.7a5b5154.chunk.js"
+  },
+  {
+    "revision": "6f394f356deefd32576d",
+    "url": "/my_site/static/js/runtime-main.5d6d82bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_site/static/media/logo.5d5d9eef.svg"
+  }
+]);
